6c0f7b0657e70bf975cd68d776c7c305785e7a33
damarma <dmartimarti@gmail.com>
2021-11-09 10:46:31 +0000

    Update Respiration_sublibrary_analysis.R

--- Respiration_sublibrary_analysis.R
+++ Respiration_sublibrary_analysis.R
@@ -95,6 +95,82 @@ ggsave(file = here('Summary', 'Scatter_sub_lib_RESP_5uM.pdf'),
 
 
 
+# Tanara version ----------------------------------------------------------
+
+
+respT = read_excel("Sub_library/Tanara/res_sublibrary_exp090821.xlsx", 
+                   sheet = "Summary")
+# remove bad data
+respT = respT %>% 
+  mutate(`0` = as.numeric(`0`),
+         `5` = as.numeric(`5`)) %>% 
+  drop_na(Genes) %>% 
+  filter(Genes != 'EMPTY')
+
+
+respT
+
+
+
+respT = respT %>%
+  gather(Drug, Score, `0`, `5`) %>% # make table long
+  unite(ID, Genes, Supplement, Replicate, remove = FALSE) %>% 
+  mutate(Replicate = as.numeric(Replicate),
+         Supplement_mM = as.factor(Supplement_mM),
+         Supplement = as.factor(Supplement),
+         Genes = as.factor(Genes),
+         ID = as.factor(ID),
+         Drug = as.factor(Drug)) %>%
+  select(Genes, Well, ID, Replicate, Supplement, Supplement_mM, Drug, Score)
+
+
+
+respT.sum = respT %>%
+  group_by(Supplement, Supplement_mM, Drug, Genes) %>%
+  summarise(Median_Score = median(Score, na.rm = TRUE),
+            MAD = mad(Score, na.rm = TRUE),
+            Mean = mean(Score, na.rm = TRUE),
+            SD = sd(Score, na.rm = TRUE)) %>%
+  mutate(BW_Score = Median_Score[Genes == 'BW'],
+         BW_Mean = Mean[Genes == 'BW']) %>%
+  ungroup %>%
+  group_by(Genes, Drug) %>%
+  ungroup %>%
+  mutate(BW_norm = Median_Score - BW_Score,
+         BW_Mean_norm = Mean - BW_Mean)
+
+
+
+
+drug = 5
+pos = position_jitter(width = 0.05, height = 0.05, seed = 1) # to plot names in jitter positions
+respT.sum %>% 
+  filter(Drug == drug) %>% 
+  select(Supplement, Supplement_mM, Genes, BW_norm) %>%
+  unite(Supp, Supplement, Supplement_mM) %>%
+  spread(Supp, BW_norm) %>%
+  ggplot(aes(x = Glucose_0, y = Glucose_10)) + 
+  geom_hline(yintercept = 0, colour = 'grey30') +
+  geom_vline(xintercept = 0, colour = 'grey30') +
+  geom_point(position = pos, size = 2) + 
+  geom_text_repel(aes(label = Genes), position = pos, max.overlaps = 100) +
+  labs(title = expression(paste("5FU + Glucose effect on ", italic('C. elegans'),
+                                " N2 phenotype", sep = '')),
+       x = "Normalised median scores of *C. elegans* N2 phenotype",
+       y = "Normalised median scores of *C. elegans* N2 phenotype with **Glucose**") +
+  theme(plot.title = element_text(size = 15, hjust = 0.5, face = "bold"),
+        panel.grid.major = element_line(colour = "grey90"),
+        panel.background = element_rect(fill = "white", colour = "grey50"),
+        legend.text = element_text(size = 6),
+        axis.title.y = ggtext::element_markdown(),
+        axis.title.x = ggtext::element_markdown()) + 
+  guides(colour = guide_legend(override.aes = list(size = 4))) 
+
+
+ggsave(file = here('Summary', 'Scatter_sub_lib_RESP_5uM.pdf'),
+       height = 10, width = 12)
+
+
 # compare old and new glu -------------------------------------------------
 
 
