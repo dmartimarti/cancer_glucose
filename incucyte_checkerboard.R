a27dbeb6c1287fb97ee3c2c7ecc4306646e1cbdf
damarma <dmartimarti@gmail.com>
2022-02-16 13:25:36 +0000

    Update incucyte_checkerboard.R

--- incucyte_checkerboard.R
+++ incucyte_checkerboard.R
@@ -6,6 +6,7 @@ library(cowplot)
 library(here)
 library(readxl)
 library(DescTools)
+library(viridisLite)
 
 
 theme_set(theme_cowplot(15))
@@ -21,15 +22,22 @@ clean_data = read_excel("clean_data.xlsx",
 clean_data = clean_data %>% 
   pivot_longer(cols = `0`:`10`, 
                names_to = 'FU', values_to = 'confluence') %>% 
-  mutate(Micit = factor(Micit, levels = c(0,1,5,10)),
+  mutate(Micit = factor(Micit, levels = c(10, 5, 1, 0)),
          FU = factor(FU, levels = c(0, 0.04, 0.08, 0.16, 0.31, 0.63, 1.25, 2.5, 5, 10)))
 
 
 # plot 
 clean_data %>% 
+  mutate(Elapsed = round(Elapsed/60, 1)) %>% 
   ggplot(aes(x = Elapsed, y = confluence)) +
   geom_line(aes(color = Micit)) +
-  facet_wrap(vars(Micit, FU), ncol = 10)
+  # facet_wrap(vars(Micit, FU), ncol = 10)
+  facet_grid(rows = vars(Micit), 
+             cols = vars(FU)) +
+  labs(
+    x = 'Time (hours)',
+    y = 'Confluence (%)'
+  )
 
 
 
@@ -67,6 +75,19 @@ data_viab = data_auc %>%
            pull(auc)) %>% 
   mutate(viability = ((auc/control) * 100))
 
+
+
+data_viab %>% 
+  ggplot(aes(y = Micit, x = FU, fill = viability)) +
+  geom_raster() +
+  scale_fill_viridis_c() +
+  labs(
+    x = '5-FU',
+    y = 'Micit'
+  )
+
+ggsave(here('summary', 'auc_heatmap_rep1.pdf'),
+       height = 6, width = 8)
 
 
 
